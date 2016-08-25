36e9c1aa0e56b18fc6cd6f2b9d8a0276f808f2f6
Adrien <adrien@arccoza.com>
2016-08-25 14:56:28 +0900

    New options extraction functions (extract from rsync --help) working.

--- extract.py
+++ extract.py
@@ -3,6 +3,7 @@ import json
 import pexpect
 from pprint import pprint
 
+
 def rsync_version():
   ver = None
   err = None
@@ -19,6 +20,35 @@ def rsync_version():
       err = 'command not found'
     else:
       err = u'unknown'
+  return err, ver
 
 
-rsync_version()
+pprint(rsync_version())
+re_doc_opts = re.compile(u'(?:(?:^\s-(?P<short>[^-\s,]),?)|(^\s*))(?:(?:\s--(?:(?:(?P<long>[^\s=]+)=?(?P<value>[^-\s]+)?(?:\s*(?P<desc>.*$)))))|(?:\s*same as(?P<same_as>\s--.*$)*))', re.MULTILINE)
+
+def rsync_options():
+  """Get rsync docs from the rsync command line."""
+  rsync = pexpect.spawn(u'rsync --help')
+  i = rsync.expect(pexpect.EOF)
+  options = parse_rsync_options(rsync.before)
+  pprint(options)
+  
+
+def parse_rsync_options(text):
+  """Extract valid argument options from rsync docs."""
+  short = {}
+  long = {}
+  for match in re_doc_opts.finditer(text):
+    g = match.groupdict()
+
+    # Split the same_as into individual long options, if it is found. (Couldn't be done in the regex).
+    g[u'same_as'] = filter(None, g[u'same_as'].split(u' --')) if g[u'same_as'] else None
+
+    if g[u'short']:
+      short[g[u'short']] = {u'long': g[u'long'], u'value': g.get(u'value'), u'same_as':g[u'same_as'], u'desc': g[u'desc']}
+    if g[u'long']:
+      long[g[u'long']] = {u'short': g[u'short'], u'value': g.get(u'value'), u'desc': g[u'desc']}
+
+  return short, long
+
+rsync_options()
